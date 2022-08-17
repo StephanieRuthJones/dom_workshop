fc5c98c37aa8cd7b0048a726207db6a905220690
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-08-17 16:07:33 -0600

    Update DOM Assessment instructions and boilerplate code

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## How to run the web app?
 
 0. Clone the repo
-1. Install dependencies (Express server) `$ npm install`
-2. Start the server `npm start`
-3. Navigate to `localhost:3000` in browser`
+1. Install dependencies `npm install`
+2. Start lite-server with `npm start`
---
+++ app.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM has been fully loaded')
+    console.table(animals)
+
+    //select header by id
+
+    //change color of header text
+
+    //select card container div by class
+
+    //map through the data
+    animals.map(animal => {
+        //create animalName, animalDescription, and animalImage variables to reference each piece of data
+
+        //create <div> element for card
+
+
+
+        //add "card" class to card div
+        //write code on whiteboard
+
+
+        //append "card" div to cardContainer
+
+
+        //create image element
+
+
+        //console.log image
+
+        //add class name attribute to image element
+
+        //console.log image
+
+        //add src to image element
+        //write code on whiteboard
+
+
+        //append image to parent (card div)
+
+        //Check to ensure images rendered w console.log
+
+        //create div with class "card-body"
+
+
+        //give card-body div a class of "card-body"
+
+
+        //append card-body div to card div
+
+
+        //create h5 tag for card title
+
+
+        //add "card-title" class to card title
+
+
+        //add title of animal's name
+
+
+        //append h5 tag to card body div
+
+
+
+        //can stop here unless you would like to keep going
+
+        //create p tag 
+
+        //add "card-text" class to p tag
+
+        //add inner HTML of animal description to p tag
+
+        //append p tag to card-body
+
+        //create a element
+
+
+        //add class of "btn btn-primary"
+
+
+        //add inner HTML of "Go somewhere"
+
+
+        //append button to card-body
+
+    })
+})
